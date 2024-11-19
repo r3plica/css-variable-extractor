da9e8a877f0f8f7c3039e43ef92d2d1984d3b734
Jaymie Jeffrey <jaymie@r3plica.co.uk>
2024-11-19 19:14:15 +0000

    Updated release start script

--- scripts/release-start.sh
+++ scripts/release-start.sh
@@ -10,16 +10,9 @@ if [ -z "$new_version" ]; then
   exit 1
 fi
 
-release_branch="release/$new_version"
-echo "Starting new release branch: $release_branch"
-git flow release start "$release_branch"
+echo "Starting new release branch: $new_version"
+git flow release start "$new_version"
 
-git rev-parse --verify "$release_branch" > /dev/null
-if [ $? -ne 0 ]; then
-  echo "Failed to create release branch!"
-  exit 1
-fi
-
-echo "Release branch $release_branch started with version $new_version"
+echo "Release branch $new_version started"
 
 exit 0
