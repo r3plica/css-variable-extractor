f859fcf583d509a0896d2eea3e70d5265a74d072
Jaymie Jeffrey <jaymie@r3plica.co.uk>
2024-11-21 22:44:31 +0000

    Fixed unit tests for the new colours component and store

--- src/app/_core/components/footer/footer.component.spec.ts
+++ src/app/_core/components/footer/footer.component.spec.ts
@@ -7,7 +7,6 @@ describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
-  // Assemble
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [FooterComponent],
@@ -19,47 +18,35 @@ describe('FooterComponent', () => {
   });
 
   it('should create', () => {
-    // Act
     const compiled = fixture.nativeElement;
-
-    // Assert
     expect(component).toBeTruthy();
     expect(compiled).toBeTruthy();
   });
 
   it('should display the current year', () => {
-    // Act
     fixture.detectChanges();
     const yearElement = fixture.debugElement.query(
       By.css('.text-sm'),
     ).nativeElement;
-
-    // Assert
     expect(yearElement.textContent).toContain(
       new Date().getFullYear().toString(),
     );
   });
 
   it('should display the correct copyright text', () => {
-    // Act
     fixture.detectChanges();
     const copyrightElement = fixture.debugElement.query(
       By.css('.text-sm'),
     ).nativeElement;
-
-    // Assert
-    expect(copyrightElement.textContent).toContain('Notus Angular by');
-    expect(copyrightElement.textContent).toContain('Creative Tim');
+    expect(copyrightElement.textContent).toContain('Copyright ©');
+    expect(copyrightElement.textContent).toContain('r3plica Ltd');
   });
 
-  it('should have a link to Creative Tim', () => {
-    // Act
+  it('should have a link to r3plica Ltd', () => {
     fixture.detectChanges();
     const linkElement = fixture.debugElement.query(By.css('a')).nativeElement;
-
-    // Assert
     expect(linkElement.href).toContain(
-      'https://www.creative-tim.com/?ref=na-footer',
+      'https://www.r3plica.co.uk/?ref=na-footer',
     );
   });
 });
