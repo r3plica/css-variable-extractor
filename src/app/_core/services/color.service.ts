751fc5087b17933f1d28e347a48ad97abd685fd1
Jaymie Jeffrey <jaymie@r3plica.co.uk>
2024-11-22 14:36:14 +0000

    Removed comments

--- src/app/_core/services/color.service.ts
+++ src/app/_core/services/color.service.ts
@@ -9,13 +9,23 @@ import { CssVariable } from '@models';
 export class ColorService {
   public generateColorScale(variables: CssVariable[]): CssVariable[] {
     const colorScale: CssVariable[] = [];
+    const existingVariables = new Map(
+      variables.map((variable) => [variable.name, variable.value]),
+    );
 
-    variables.forEach((variable) => {
-      const { name, value } = variable;
+    const baseVariables = this.getBaseVariables(variables);
+
+    baseVariables.forEach((baseVariable) => {
+      const { name, value } = baseVariable;
       if (!this.isValidHex(value)) return;
 
       const readableName = name.replace(/^--/, '');
-      this.generateColorIncrements(value, readableName, colorScale);
+      this.generateColorIncrements(
+        value,
+        readableName,
+        colorScale,
+        existingVariables,
+      );
     });
 
     return colorScale;
@@ -29,6 +39,7 @@ export class ColorService {
     hexColor: string,
     readableName: string,
     colorScale: CssVariable[],
+    existingVariables: Map<string, string>,
   ) {
     const scaleColors = scale([
       chroma(hexColor).set('hsl.l', 0.6),
@@ -40,14 +51,32 @@ export class ColorService {
 
     scaleColors.forEach((color, index) => {
       const incrementValue = 50 + index * 50;
-      const incrementName = `--${readableName}-${incrementValue.toString()}`;
-      if (colorScale.find((variable) => variable.name === incrementName))
-        return;
+      const incrementName = `--${readableName}-${incrementValue}`;
 
-      colorScale.push({
-        name: incrementName,
-        value: color,
-      });
+      if (existingVariables.has(incrementName)) {
+        colorScale.push({
+          name: incrementName,
+          value: existingVariables.get(incrementName)!,
+        });
+      } else {
+        colorScale.push({
+          name: incrementName,
+          value: color,
+        });
+        existingVariables.set(incrementName, color);
+      }
+    });
+  }
+
+  private getBaseVariables(variables: CssVariable[]): CssVariable[] {
+    const increments = Array.from({ length: 19 }, (_, i) => (i + 1) * 50);
+
+    return variables.filter((variable) => {
+      const match = variable.name.match(/-(\d+)$/);
+      if (!match) return true;
+
+      const incrementValue = parseInt(match[1], 10);
+      return !increments.includes(incrementValue);
     });
   }
 }
